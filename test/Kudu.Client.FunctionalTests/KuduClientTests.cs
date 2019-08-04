1db6bff66b225fab25bd06b98f6314ea8683fe55
plemanach <plemanach@hotmail.com>
2019-08-04 11:54:07 +0100

    Add kuduclient tests

---
+++ test/Kudu.Client.FunctionalTests/KuduClientTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Kudu.Client;
+using Kudu.Client.FunctionalTests.MiniCluster;
+using McMaster.Extensions.Xunit;
+using Xunit;
+
+namespace Kudu.Client.FunctionalTests
+{
+    public class KuduClientTests : MiniKuduClusterTestBase
+    {
+        [SkippableFact]
+        public async Task BadHostnames()
+        {
+            const string badHostname = "some-unknown-host-hopefully";
+            //arrange
+            var kuduClient = KuduClient.Build(badHostname);
+            // act & assert
+            await Assert.ThrowsAnyAsync<SocketException>( () => kuduClient.ListTabletServersAsync());
+           
+        }
+    }
+}
